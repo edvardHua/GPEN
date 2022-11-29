85904ab1de4cea6b1c1c22bae0bf60d0a03b02c9
zihua.zeng <zihua.zeng@shopee.com>
2022-11-29 16:11:19 +0800

    fixed bug

--- train_simple.py
+++ train_simple.py
@@ -367,8 +367,8 @@ if __name__ == '__main__':
 
     if args.distributed:
         torch.cuda.set_device(args.local_rank)
-    torch.distributed.init_process_group(backend='nccl', init_method='env://')
-    synchronize()
+        torch.distributed.init_process_group(backend='nccl', init_method='env://')
+        synchronize()
 
     args.latent = 512
     args.n_mlp = 8
@@ -413,15 +413,13 @@ if __name__ == '__main__':
 
     if args.pretrain is not None:
         print('load model:', args.pretrain)
+        ckpt = torch.load(args.pretrain)
+        generator.load_state_dict(ckpt['g'])
+        discriminator.load_state_dict(ckpt['d'])
+        g_ema.load_state_dict(ckpt['g_ema'])
 
-    ckpt = torch.load(args.pretrain)
-
-    generator.load_state_dict(ckpt['g'])
-    discriminator.load_state_dict(ckpt['d'])
-    g_ema.load_state_dict(ckpt['g_ema'])
-
-    g_optim.load_state_dict(ckpt['g_optim'])
-    d_optim.load_state_dict(ckpt['d_optim'])
+        g_optim.load_state_dict(ckpt['g_optim'])
+        d_optim.load_state_dict(ckpt['d_optim'])
 
     smooth_l1_loss = torch.nn.SmoothL1Loss().to(device)
     id_loss = IDLoss(args.base_dir, device, ckpt_dict=None)
@@ -434,20 +432,20 @@ if __name__ == '__main__':
             output_device=args.local_rank,
             broadcast_buffers=False,
         )
+        discriminator = nn.parallel.DistributedDataParallel(
+            discriminator,
+            device_ids=[args.local_rank],
+            output_device=args.local_rank,
+            broadcast_buffers=False,
+        )
 
-    discriminator = nn.parallel.DistributedDataParallel(
-        discriminator,
-        device_ids=[args.local_rank],
-        output_device=args.local_rank,
-        broadcast_buffers=False,
-    )
+        id_loss = nn.parallel.DistributedDataParallel(
+            id_loss,
+            device_ids=[args.local_rank],
+            output_device=args.local_rank,
+            broadcast_buffers=False,
+        )
 
-    id_loss = nn.parallel.DistributedDataParallel(
-        id_loss,
-        device_ids=[args.local_rank],
-        output_device=args.local_rank,
-        broadcast_buffers=False,
-    )
     if args.split_hor:
         dataset = FaceDataset(args.path, args.size, True)
     else:
